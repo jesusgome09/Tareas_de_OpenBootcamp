673732de92d14bb2f7541054609733fef1a4ba78
Jesus Gomez <72950323+jesusgome09@users.noreply.github.com>
2023-02-26 22:03:55 -0500

    Tarea2 Introducción a la programación

--- Main.java
+++ Main.java
@@ -1,26 +1,49 @@
-//Creado por: Jesus Gomez
+package Tarea2_Introduccion;
 
 public class Main {
     public static void main(String[] args) {
-        //primera parte
-        System.out.println(suma(5,10,15));
+        int numeroif = 3;
+        if(numeroif >= 1){
+            System.out.println("numeroif es positivo");
+        }
+        else if(numeroif == 0){
+            System.out.println("numeroif es cero");
+        }
+        else if (numeroif <= -1) {
+            System.out.println("numeroif es negativo");
+        } else {
+            System.out.println("error");
+        }
+        int numeroWhile = 0;
+        while (numeroWhile < 3){
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+        do{
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }while (numeroWhile < 3);
+        for (int numeroFor = 0; numeroFor <= 3 ; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+        String estacion = "verano";
+        switch (estacion){
+            case "verano":
+                System.out.println("estamos en " + estacion);
+                break;
+            case "primavera":
+                System.out.println("estamos en " + estacion);
+                break;
+            case "otoño":
+                System.out.println("estamos en " + estacion);
+                break;
+            case "invierno":
+                System.out.println("estamos en " + estacion);
+                break;
+            default:
+                break;
 
-        //segunda parte
-        coche miCoche = new coche();
-        miCoche.agregarPuerta();
-        System.out.println(miCoche.puertas);
-    }
-    public static int suma(int a, int b, int c)
-    {
-        return a+b+c;
-    }
-}
-class coche
-{
-    int puertas = 0;
-    public void agregarPuerta()
-    {
-        puertas++;
+        }
     }
 
 }
